3383743e728e03dc03a56a45b1566e5688dc8075
dngwoodo <dngwoodo@gmail.com>
2021-07-02 14:03:16 +0900

    feat: Show title of posts

--- pages/index.test.tsx
+++ pages/index.test.tsx
@@ -1,11 +1,26 @@
 import { render } from '@testing-library/react';
 
-import Home from './Home';
+import Home from '.';
 
 describe('Home', () => {
-  it('renders component name', () => {
-    const { getByText } = render(<Home />);
+  it('renders post titles', () => {
+    const { getByText } = render(
+      <Home
+        posts={[
+          {
+            slug: 'writing-great-unit-tests',
+            frontmatter: {
+              title: 'Writing Great Unit Tests',
+              date: 'March 12, 2021',
+              excerpt:
+                'We will look at 10 simple tips and tricks on writing unit tests in JavaScript',
+              coverImage: '/images/posts/img7.jpg',
+            },
+          },
+        ]}
+      />
+    );
 
-    expect(getByText('Blog')).toBeInTheDocument();
+    expect(getByText('Writing Great Unit Tests')).toBeInTheDocument();
   });
 });
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,52 @@
-import Home from './Home';
+import fs from 'fs';
+import path from 'path';
 
-export default Home;
+import Head from 'next/head';
+import matter from 'gray-matter';
+
+import { GetStaticProps } from 'next';
+
+type Props = {
+  posts: {
+    slug: string;
+    frontmatter: {
+      title: string;
+      date: string;
+      excerpt: string;
+      coverImage: string;
+    };
+  }[];
+};
+
+export default function Home({ posts }: Props) {
+  return (
+    <div>
+      <Head>
+        <title>Dev Blog</title>
+      </Head>
+      <ul>
+        {posts &&
+          posts.map((post) => (
+            <li key={post.slug}>{post.frontmatter.title}</li>
+          ))}
+      </ul>
+    </div>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const posts = files.map((filename) => {
+    const slug = filename.replace('.md', '');
+    const markdownWithMeta = fs.readFileSync(
+      path.join('posts', filename),
+      'utf-8'
+    );
+    const { data: frontmatter } = matter(markdownWithMeta);
+
+    return { slug, frontmatter };
+  });
+
+  return { props: { posts } };
+};
