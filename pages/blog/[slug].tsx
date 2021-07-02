f775e8589b281c694be85081d712afe94d80a203
dngwoodo <dngwoodo@gmail.com>
2021-07-02 17:17:41 +0900

    feat: Add dynamic blog page component

---
+++ pages/blog/[slug].tsx
@@ -0,0 +1,65 @@
+import fs from 'fs';
+import path from 'path';
+
+import matter from 'gray-matter';
+import marked from 'marked';
+import Link from 'next/link';
+
+import PostType from '@/types/PostType';
+import { GetStaticPaths, GetStaticProps } from 'next';
+
+type Props = PostType & { content: string };
+
+export default function PostPage({ frontmatter, content }: Props) {
+  const { title, date, coverImage } = frontmatter;
+
+  return (
+    <>
+      <Link href='/'>
+        <a href='replace'>Go Back</a>
+      </Link>
+      <div>
+        <h1>{title}</h1>
+        <div>Posted on {date}</div>
+        <img src={coverImage} alt='' />
+        <div>
+          <div dangerouslySetInnerHTML={{ __html: marked(content) }} />
+        </div>
+      </div>
+    </>
+  );
+}
+
+export const getStaticPaths: GetStaticPaths = () => {
+  const files = fs.readdirSync(path.join('posts'));
+
+  const paths = files.map((filename) => ({
+    params: {
+      slug: filename.replace('.md', ''),
+    },
+  }));
+
+  return {
+    paths,
+    fallback: false,
+  };
+};
+
+export const getStaticProps: GetStaticProps = ({ params }) => {
+  const { slug } = params!;
+
+  const markdownWithMeta = fs.readFileSync(
+    path.join('posts', `${slug}.md`),
+    'utf-8'
+  );
+
+  const { data: frontmatter, content } = matter(markdownWithMeta);
+
+  return {
+    props: {
+      slug,
+      frontmatter,
+      content,
+    },
+  };
+};
