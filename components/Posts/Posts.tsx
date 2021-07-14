90b573479d2aa2f2ef28351012f1c28b796e7008
dngwoodo <dngwoodo@gmail.com>
2021-07-14 22:08:42 +0900

    fix: Fix typo

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -1,8 +1,9 @@
 import styled from '@emotion/styled';
+import { motion } from 'framer-motion';
 
 import PostType from '@/types/PostType';
 
-import mediaquey from '@/utils/mediaquery';
+import mediaquery from '@/utils/mediaquery';
 
 import Post from '@/components/Post';
 
@@ -11,16 +12,31 @@ type Props = {
 };
 
 const Cards = styled.ul(
-  mediaquey({
+  mediaquery({
     display: ['block', 'flex'],
     flexWrap: 'wrap',
   })
 );
 
+const MotionDiv = styled(motion.div)({
+  width: '300px',
+  height: '300px',
+  background: 'red',
+});
+
 export default function Posts({ posts }: Props) {
   return (
-    <Cards>
-      {posts && posts.map((post) => <Post key={post.slug} post={post} />)}
-    </Cards>
+    <>
+      <Cards>
+        {posts && posts.map((post) => <Post key={post.slug} post={post} />)}
+      </Cards>
+      <MotionDiv
+        animate={{
+          scale: [1, 2, 2, 1, 1],
+          rotate: [0, 0, 270, 270, 0],
+          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
+        }}
+      />
+    </>
   );
 }
