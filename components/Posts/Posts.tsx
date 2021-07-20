b816e6036211f56581e193b7f0d48fe904c8aca0
dngwoodo <dngwoodo@gmail.com>
2021-07-20 17:35:11 +0900

    feat: Remove motionDiv component
    필요없는 컴포넌트 삭제

--- components/Posts/Posts.tsx
+++ components/Posts/Posts.tsx
@@ -1,5 +1,4 @@
 import styled from '@emotion/styled';
-import { motion } from 'framer-motion';
 
 import PostType from '@/types/PostType';
 
@@ -18,25 +17,12 @@ const Cards = styled.ul(
   })
 );
 
-const MotionDiv = styled(motion.div)({
-  width: '300px',
-  height: '300px',
-  background: 'red',
-});
-
 export default function Posts({ posts }: Props) {
   return (
     <>
       <Cards>
         {posts && posts.map((post) => <Post key={post.slug} post={post} />)}
       </Cards>
-      <MotionDiv
-        animate={{
-          scale: [1, 2, 2, 1, 1],
-          rotate: [0, 0, 270, 270, 0],
-          borderRadius: ['20%', '20%', '50%', '50%', '20%'],
-        }}
-      />
     </>
   );
 }
