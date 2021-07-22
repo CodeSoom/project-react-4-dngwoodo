31ffb5c4487aa338c884e2b2df537eb33a860a2a
dngwoodo <dngwoodo@gmail.com>
2021-07-22 23:06:04 +0900

    env: Setup firebase auth login
    Install firebase
    Setup firebase auth login
    Add alias path of services

---
+++ containers/LoginContainer/LoginContainer.tsx
@@ -0,0 +1,15 @@
+export default function LoginContainer() {
+  return (
+    <div style={{ paddingTop: '150px' }}>
+      <h1>Login</h1>
+      <ul>
+        <li>
+          <button type='button'>Google</button>
+        </li>
+        <li>
+          <button type='button'>Github</button>
+        </li>
+      </ul>
+    </div>
+  );
+}
