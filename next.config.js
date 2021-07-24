8cd9b0c5af2c6730346298b9515f54998a42d561
dngwoodo <dngwoodo@gmail.com>
2021-07-24 16:25:14 +0900

    env: Add image\'s domain of github

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   pageExtensions: ['page.tsx'],
   images: {
-    domains: ['lh3.googleusercontent.com'],
+    domains: ['lh3.googleusercontent.com', 'avatars.githubusercontent.com'],
   },
 };
