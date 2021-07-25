79acb306705f5b8cb054093f37f6f4adaae3a97b
dngwoodo <dngwoodo@gmail.com>
2021-07-25 21:56:14 +0900

    env: Create readPost function
    firebase의 realdatabase에서 데이터를 읽어오는 함수 생성

--- services/post-repository.ts
+++ services/post-repository.ts
@@ -1,16 +1,28 @@
 import { dbService } from './firebase';
 
 type Post = {
+  postId: string;
   title: string;
   body: string;
   like: number;
   thumbnail: string;
+  createdAt: string;
+  updatedAt: string;
 };
 
 export function savePost(postData: Post) {
   const newPostKey = dbService.ref().child('posts').push().key;
 
-  return dbService.ref().update({ [`/posts/${newPostKey}`]: postData });
+  dbService.ref().update({ [`/posts/${newPostKey}`]: postData });
 }
 
-export default {};
+export function readPost(postId: string) {
+  const postRef = dbService.ref(`posts/${postId}`);
+  let result;
+
+  postRef.on('value', (snapshot) => {
+    result = snapshot.val();
+  });
+
+  return result;
+}
