dba76e3a6ef3c3f370e604b9edbf1d9d9fd44566
dngwoodo <dngwoodo@gmail.com>
2021-07-25 21:27:39 +0900

    env: Create savePost function
    firebase의 realdatabase에 데이터를 저장하는 함수 생성

--- services/firebase.ts
+++ services/firebase.ts
@@ -1,10 +1,12 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
+import 'firebase/database';
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
   projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseURL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
 };
 
 function initializeFirebaseApp() {
@@ -20,3 +22,4 @@ const firebaseApp = initializeFirebaseApp();
 export const firebaseAuth = firebaseApp.auth();
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 export const githubProvider = new firebase.auth.GithubAuthProvider();
+export const dbService = firebase.database();
